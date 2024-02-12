36ca6f82147d90d7ae7704427460c53238a9b894
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2024-02-12 19:23:25 +0530

    "Signup comoponent created"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Login from './components/Login'
+import Signup from './components/Signup'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Login></Login>
+      {/* <Login></Login> */}
+      <Signup></Signup>
     </>
   )
 }
---
+++ src/components/Signup.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+function Signup() {
+
+    const onSubmitHandler = ()=> {
+
+    }
+
+  return (
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+       <form 
+       className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+       onSubmit={onSubmitHandler}>
+        <div>
+        <h1 className='text-center text-xl rounded font-bold mb-2'>Sign up</h1>
+        </div>
+        <div className='mb-4'>
+            <label className="block text-gray-700 text-left text-sm font-bold mb-2" >
+                Username
+            </label>
+            <input 
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder='Enter your Username' 
+            value =''
+
+            />
+        </div>
+        <div className='mb-4'>
+            <label className="block text-gray-700 text-left text-sm font-bold mb-2" >
+                Email
+            </label>
+            <input 
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="email"
+            placeholder='Enter your Email' 
+            value =''
+
+            />
+        </div>
+        <div className='mb-4'>
+            <label className="block text-gray-700 text-left text-sm font-bold mb-1.5" >
+                Password
+            </label>
+            <input 
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="password" 
+            placeholder='Enter your password'
+            value=''
+            />
+        </div>
+        <div className='mb-4'>
+            <label className="block text-gray-700 text-left text-sm font-bold mb-2" >
+                Confirm Password
+            </label>
+            <input 
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="email"
+            placeholder='Confirm your password' 
+            value =''
+
+            />
+        </div>
+        
+        <div>
+            <button className="bg-purple-500 hover:bg-blue-700 w-full text-white font-bold py-2 px-4 rounded-full">
+                Sign Up
+            </button>
+        </div>
+        
+       </form>
+    </div>
+  )
+}
+
+export default Signup
