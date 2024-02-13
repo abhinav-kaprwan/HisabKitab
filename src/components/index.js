72bb9b01b61e9220b00c70e176c82ab63ed146e3
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2024-02-13 19:30:01 +0530

    Added routing and created aHeader component

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Login from "./Login";
+import Signup from "./Signup";
+import Header from "./Header";
+
+export {Login,Header,Signup}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {Login,Signup } from './components'
+import Layout from './Layout'
+import { createBrowserRouter ,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      
+      <Route path='signup' element={<Signup/>}/>
+      <Route path='login' element={<Login/>}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
