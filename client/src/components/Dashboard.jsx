03578bfd9d0231d77b86af6e19da5e2f70d02da3
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2024-03-04 22:09:03 +0530

    "Props problem solved"

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {Button} from "./ui/button"
+import SideNavbar from './SideNavbar'
 function Dashboard() {
   return (
     <>
-   Dashboard
+   <Button>Hello shadcn</Button>
+   <SideNavbar></SideNavbar>
     </>
   )
 }
